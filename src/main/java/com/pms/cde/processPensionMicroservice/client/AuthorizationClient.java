f0e5840cf5e1b3eef8d0be8b4423c941776ef37e
Prashant Singh <52065013+PrashantTariyal@users.noreply.github.com>
2022-08-22 10:51:53 +0530

    Update AuthorizationClient.java

--- src/main/java/com/pms/cde/processPensionMicroservice/client/AuthorizationClient.java
+++ src/main/java/com/pms/cde/processPensionMicroservice/client/AuthorizationClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "auth-service",url = "http://65.1.111.103:7001/auth")
+@FeignClient(name = "auth-service",url = "http://3.110.108.65:7001/auth")
 public interface AuthorizationClient {
 	@PostMapping(value = "/authorize")
 	public boolean authorizeRequest(@RequestHeader(value = "Authorization",required = true) String header);
